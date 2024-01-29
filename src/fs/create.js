824f5e85c85b48bdba497563c655e6b2b3981634
Siarhei Siarheyeu <95449526+awer17@users.noreply.github.com>
2024-01-29 00:06:52 -0700

    feat: create finish

--- src/fs/create.js
+++ src/fs/create.js
@@ -6,25 +6,11 @@ const create = async () => {
     const folderName = 'files';
     const filename = path.join(folderName, 'fresh.txt');
 
-    let fileFlag = true;
-
     try {
-        // Check if the file already exists asynchronously
         await fs.access(filename);
-
-        // If access does not throw an error, then the file exists
         console.log(`File '${filename}' already exists. FS operation failed.`);
-        fileFlag = false;
     } catch (notError) {
-        // File does not exist
-    }
-
-    if (fileFlag) {
         try {
-            // Create the folder if it doesn't exist
-            await fs.mkdir(folderName, { recursive: true });
-
-            // Create the file
             await fs.writeFile(filename, content);
             console.log('File created successfully!');
         } catch (writeError) {
@@ -32,8 +18,6 @@ const create = async () => {
         }
     }
 };
-
-// Call the create function
 create();
 
 
--- src/fs/dist/create.dev.js
+++ src/fs/dist/create.dev.js
@@ -7,7 +7,7 @@ var _path = _interopRequireDefault(require("path"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var create = function create() {
-  var content, folderName, filename, fileFlag;
+  var content, folderName, filename;
   return regeneratorRuntime.async(function create$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
@@ -15,55 +15,38 @@ var create = function create() {
           content = 'I am fresh and young inside';
           folderName = 'files';
           filename = _path["default"].join(folderName, 'fresh.txt');
-          fileFlag = true;
-          _context.prev = 4;
-          _context.next = 7;
+          _context.prev = 3;
+          _context.next = 6;
           return regeneratorRuntime.awrap(_promises["default"].access(filename));
 
-        case 7:
-          // If access does not throw an error, then the file exists
+        case 6:
           console.log("File '".concat(filename, "' already exists. FS operation failed."));
-          fileFlag = false;
-          _context.next = 13;
+          _context.next = 20;
           break;
 
-        case 11:
+        case 9:
+          _context.prev = 9;
+          _context.t0 = _context["catch"](3);
           _context.prev = 11;
-          _context.t0 = _context["catch"](4);
-
-        case 13:
-          if (!fileFlag) {
-            _context.next = 25;
-            break;
-          }
-
-          _context.prev = 14;
-          _context.next = 17;
-          return regeneratorRuntime.awrap(_promises["default"].mkdir(folderName, {
-            recursive: true
-          }));
-
-        case 17:
-          _context.next = 19;
+          _context.next = 14;
           return regeneratorRuntime.awrap(_promises["default"].writeFile(filename, content));
 
-        case 19:
+        case 14:
           console.log('File created successfully!');
-          _context.next = 25;
+          _context.next = 20;
           break;
 
-        case 22:
-          _context.prev = 22;
-          _context.t1 = _context["catch"](14);
+        case 17:
+          _context.prev = 17;
+          _context.t1 = _context["catch"](11);
           console.error('Error creating file:', _context.t1);
 
-        case 25:
+        case 20:
         case "end":
           return _context.stop();
       }
     }
-  }, null, null, [[4, 11], [14, 22]]);
-}; // Call the create function
-
+  }, null, null, [[3, 9], [11, 17]]);
+};
 
 create();
